d2129ed136ac5942ab62d7250c5e9665fcc140ed
Rutvik J <rutvikj77@outlook.com>
2021-10-19 17:01:28 +0530

    Updates

---
+++ precache-manifest.35b1e0fb6a229bc818db59e0e26564e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54d562665ee3dcadaef942bf2f4879a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "11038db9e41b9776725a",
+    "url": "/home/static/css/main.8bd5fbd2.chunk.css"
+  },
+  {
+    "revision": "1a65a7da1e8939f3bc20",
+    "url": "/home/static/js/2.0e128136.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.0e128136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11038db9e41b9776725a",
+    "url": "/home/static/js/main.b1e8be25.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "9c9d896e00db5c9e5da64e0ef1dcc184",
+    "url": "/home/static/media/resume.9c9d896e.pdf"
+  },
+  {
+    "revision": "58aae4d031e424fe26e96dca1024b9c9",
+    "url": "/home/static/media/rutvik-j.58aae4d0.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.35b1e0fb6a229bc818db59e0e26564e8.js"
 );
 
 self.addEventListener('message', (event) => {
